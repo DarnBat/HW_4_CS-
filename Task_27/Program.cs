421e8300d764b8435fc362e161e359dc9f3bc642
Vladislav.M <lidpid@gmail.com>
2022-12-30 00:56:43 +0600

    task 27 completed

---
+++ Task_27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine()), result = 0;
+while(n / 10 > 0){
+    result = n % 10 + result;
+    n /= 10;
+}
+result += n;
+Console.WriteLine($"Сумма цифр в числе: {result}");
