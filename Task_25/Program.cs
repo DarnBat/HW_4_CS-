451717189867bcbfa63867f5298dba5ee883381e
Vladislav.M <lidpid@gmail.com>
2022-12-30 00:38:45 +0600

    task 25 completed

---
+++ Task_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for(int i = 0; i < b; i++){
+    result *= a;
+}
+Console.WriteLine($"Результат: {result}");
