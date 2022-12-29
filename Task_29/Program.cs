117d8ed4c9b092ce57f090e13c5c3aa87cb2b527
Vladislav.M <lidpid@gmail.com>
2022-12-30 01:37:35 +0600

    task 29 completed

---
+++ Task_29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая спрашивает у пользователя кол-во элементов и спршивает сами элементы
+// После записи элементов в массив, необходимо его вывести на экран.
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+for(int i = 0; i < n; i++){
+    Console.Clear();
+    Console.WriteLine($"Введите элемент массива {i + 1}/{n}");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Clear();
+Console.WriteLine($"[{string.Join(", ", arr)}]");
